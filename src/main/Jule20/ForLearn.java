91ac06b189f7dc1af8f2b719b4283b09992c7cb1
Andry Laptop <saipeg@yandex.ru>
2020-07-25 01:29:38 +0300

    Ready task from JavaRush

---
+++ src/main/Jule20/ForLearn.java
@@ -0,0 +1,19 @@
+package main.Jule20;
+
+public class ForLearn {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i < 11; i++) {
+            System.out.println();
+            for (int j = 1; j < 11 ; j++) {
+
+                System.out.print(j*i+ " \t");
+
+            }
+
+        }
+
+    }
+
+}
